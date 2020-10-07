84515436019906756b3273b6c5e8fac15c56dead
isabelle kasas <isabellekasas@mbp-de-isabelle.home>
2020-10-07 10:25:52 +0200

    ajout de la méthode map dans ContactList

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import Contact from './component/Contact'
 import './App.css';
+import ContactList from './component/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-        name="Carl Barnett"
-        avatar="https://randomuser.me/api/portraits/men/27.jpg"
-        status="status-online"
-        text="status-text"
-        online
-      />
-      <Contact
-        name="Stacey Jensen"
-        avatar="https://randomuser.me/api/portraits/women/81.jpg"
-        status="status-offline"
-        text="status-text"
-      />
-      <Contact
-        name="Joan Lambert"
-        avatar="https://randomuser.me/api/portraits/women/43.jpg"
-        status="status-online"
-        text="status-text"
-        online
-      />
+      <ContactList />
+  
     </div>
   );
 }
--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -5,33 +5,48 @@ const users = [
     {
         name: 'Robert Reyes',
         avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-        online: false
+        online: false,
+        status: 'status-offline',
+        text: 'status-text'
     },
     {
         name: 'Nellie Caldwell',
         avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-        online: true
+        online: true,
+        status: 'status-online',
+        text: 'status-text'
     },
     {
         name: 'Vernon Mason',
         avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-        online: true
+        online: true,
+        status: 'status-online',
+        text: 'status-text'
     },
     {
         name: 'Erica Hunt',
         avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-        online: false
+        online: false,
+        status: 'status-offline',
+        text: 'status-text'
     },
     {
         name: 'Juanita Phillips',
         avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-        online: true
+        online: true,
+        status: 'status-online',
+        text: 'status-text'
     }
 ];
 
-const ContactList = () =>{
-    {users.map(users =>(
-        <Contact key={users.name} name={users.name} avatar={users.avatar} status={users.status} text={users.text} online={users.online} />
-    ))} 
-};
+const ContactList = () => (
+    <div>
+        {users.map(contact => (
+            <Contact key={contact.name} name={contact.name} avatar={contact.avatar} status={contact.status} text={contact.text} online={contact.online} />
+        ))} 
+    </div>
+);
+
+
 export default ContactList;
+
