dcf4494da72050d6bfce366e1abbb4d3c6219ef6
isabelle kasas <isabellekasas@mbp-de-isabelle.home>
2020-10-07 17:31:07 +0200

    state

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,33 +2,44 @@ import React from 'react'
 import './Contact.css'
 import PropTypes from 'prop-types'
 
-
-function Contact(props){
-    return(
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt={props.name} />
-            <div className="name"> 
-                <span> {props.name} </span>
-                <div className="status">
-                    <span className={props.status}>
-                    </span>
-                    <span className={props.text}>
-                        {props.online ? "Online" : 'Offline'}
-                    </span>
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            online : props.online
+        }
+    }
+    render(){
+        return(
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+                <div className="name">
+                    <span> {this.props.name} </span>
+                    <div className="status"
+                        onClick={event =>{
+                            const newStatus =  !this.state.online;
+                            this.setState({ online : newStatus});
+                            }
+                        }>
+                        <span className={this.state.online ? 'status-online' : 'status-offline' }></span>
+                        <span className={this.props.text}>
+                            {this.state.online ? 'Online' : 'Offline'}
+                        </span>
+                    </div>
                 </div>
             </div>
-             
-        </div>
-    );
+        );
+    }
 }
 
-Contact.prototype = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired
-}
+
+// Contact.prototype = {
+//     name: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     status: PropTypes.string.isRequired,
+//     text: PropTypes.string.isRequired,
+//     online: PropTypes.bool.isRequired
+// }
 
 export default Contact;
     
